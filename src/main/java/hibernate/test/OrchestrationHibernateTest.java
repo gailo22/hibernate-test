e088ecbb857abc0110823b5f12924b5a65a503b0
gailo22 <gailo22@gmail.com>
2012-05-05 23:58:57 +0800

    convert to maven project

--- src/main/java/hibernate/test/OrchestrationHibernateTest.java
+++ src/main/java/hibernate/test/OrchestrationHibernateTest.java
@@ -27,26 +27,33 @@ public class OrchestrationHibernateTest {
         task.setName("Task1");
         task.setDescription("Task description");
         session.save(task);
-        
+
         /* 
          * Set<OrchestrationProperty> properties = new HashSet<OrchestrationProperty>();
         properties.add(new OrchestrationProperty("doi", "delete-article"));
         properties.add(new OrchestrationProperty("prop", "delete-article-123"));
         task.setProperties(properties);
          */
-        
+
         OrchestrationProperty property1 = new OrchestrationProperty("doi", "delete-article");
         OrchestrationProperty property2 = new OrchestrationProperty("prop", "delete-article-123");
         task.addProperty(property1);
         task.addProperty(property2);
-        
+
         session.save(property1);
         session.save(property2);
 
+        OrchestrationTask task2 = (OrchestrationTask) session.get(OrchestrationTask.class, task.getId());
+
+//        OrchestrationTask task3 = (OrchestrationTask) session.load(OrchestrationTask.class, 1234L);
+//        System.out.println(task3); // throw exception
+        OrchestrationTask task4 = (OrchestrationTask) session.get(OrchestrationTask.class, 1234L);
+        System.out.println(task4); // null
+        
         session.getTransaction().commit();
-
+        
         HibernateUtil.getSessionFactory().close();
+
+        System.out.println(task2.getProperties().iterator().next().getName()); //why ok?
     }
-
-
 }
